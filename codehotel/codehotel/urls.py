31f5df4576d5419b8e8cec78949b1ca0260f701f
prem <praulayar994@gmail.com>
2024-11-18 18:32:13 +0530

    first test route create

--- codehotel/codehotel/urls.py
+++ codehotel/codehotel/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
-
+from hotels.views import test_route
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("test/", lambda r: HttpResponse("hello!!!")),
+    path("test2/", test_route)
 ]
--- codehotel/hotels/views.py
+++ codehotel/hotels/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def test_route(request):
+    return HttpResponse("Hello Prem!!")
