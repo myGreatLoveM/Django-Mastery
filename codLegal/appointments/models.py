a8f287297be031e7499e5d04c268845a591723dd
prem <praulayar994@gmail.com>
2024-11-19 16:37:39 +0530

    PracticeArea Model create, register to admin site and add unique constraint

--- codLegal/appointments/models.py
+++ codLegal/appointments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+class PracticeArea(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name="Name of Law",
+    )
+    description = models.TextField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(name="pa_name_unq", fields=("name", ))
+        ]
+
+
+
+
+
+
